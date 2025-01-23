6c6d38f79d13db5b9857ecea5ed1ed2376f5b5e6
Gaurav <gauravvishwakarma071@gmail.com>
2025-01-22 22:12:38 -0500

    Link List 1.

--- LinkList.java
+++ LinkList.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 class LinkList {
    public static void main(String args[]) {
-       LinkedList<String> list = new LinkedList<String>();
+       LinkedList<String> list = new LinkedList<>();
        list.add("is");
        list.add("a");
        list.addLast("list");
