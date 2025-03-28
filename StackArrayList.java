65297a83772ae27e39866c0571c9343fd159ffa9
Gaurav <gauravvishwakarma071@gmail.com>
2025-03-28 13:17:24 -0400

    Complete Implementation Of Stack using ArrayList.

---
+++ StackArrayList.java
@@ -0,0 +1,19 @@
+class Stack{
+
+    
+}
+
+public class StackArrayList{
+    public static void main(String[] args) {
+        
+        // Stack stack = new Stack();
+        // stack.push(5);
+        // stack.push(3);
+        // stack.push(9);
+        // while(!stack.isStackEmpty()){
+        //     System.out.println(stack.peek());
+        //     stack.pop();
+        // }
+        
+    }
+}
