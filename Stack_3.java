1b17f252ad43a6e2a7d446d8b899349f244f6a25
Gaurav <gauravvishwakarma071@gmail.com>
2025-03-28 14:22:19 -0400

    Pushing data in Stack at the bottom position and reversing it.

--- Stack_3.java
+++ Stack_3.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class Stack_3 {
 
+    //Method to push data at the bottom of the Stack.
     static void pushAtBtm(int data, Stack<Integer> stack){
 
         if(stack.isEmpty()){
@@ -11,6 +12,18 @@ public class Stack_3 {
         pushAtBtm(data, stack);
         stack.push(top);
     }
+
+    //Method to reverse complete Stack.
+    static void reverse(Stack<Integer> stack){
+        if(stack.isEmpty()){
+            return;
+        }
+        int top = stack.pop();
+        reverse(stack);
+        pushAtBtm(top, stack);
+
+    }
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
 
@@ -19,8 +32,9 @@ public class Stack_3 {
         stack.push(3);
         stack.push(9);
 
-        pushAtBtm(6, stack);
         
+        pushAtBtm(6, stack);
+        reverse(stack);
         while(!stack.isEmpty()){
             System.out.println(stack.peek());
             stack.pop();
